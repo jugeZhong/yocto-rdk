721e2168559c1b7173a728020c4b4119bb431aaa
jugeZhong <47144694@qq.com>
2025-10-25 16:32:04 +0800

    aa

--- reinit-env.sh
+++ reinit-env.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(pwd)
 
-# 移除已存在的build目录
 rm -rf build
+mkdir -p build
+cp templates/* build/ -rf
 
-
-# 初始化poky构建环境
 source poky/oe-init-build-env
 
-# 复制templates目录下的所有内容到build目录
-cp $SCRIPT_DIR/templates/conf/* $SCRIPT_DIR/build/conf -rf
+
+
