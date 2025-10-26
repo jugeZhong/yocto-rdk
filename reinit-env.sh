782c0f73a9c3b034c3f2b9df00484a1a97eb3b66
jugeZhong <47144694@qq.com>
2025-10-26 16:03:43 +0800

    111

--- reinit-env.sh
+++ reinit-env.sh
@@ -6,13 +6,9 @@ rm -rf $SCRIPT_DIR/build
 mkdir -p $SCRIPT_DIR/build
 mkdir -p $SCRIPT_DIR/out
 cp $SCRIPT_DIR/templates/* build/ -rf
-cd $SCRIPT_DIR/poky
-git checkout -b f16cffd030d21d12dd57bb95cfc310bda41f8a1f
-cd $SCRIPT_DIR/meta-openembedded
-git checkout -b e621da947048842109db1b4fd3917a02e0501aa2
 
-cd $SCRIPT_DIR
-source poky/oe-init-build-env
+cd $SCRIPT_DIR/poky
+./oe-init-build-env
 
 
 
